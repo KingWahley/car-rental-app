514f1630d194e6ad1ea9c9d964186b6ef192d1ce
KingWahley <kingwahley2@gmail.com>
2026-02-18 20:47:22 +0100

    updates

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <div className="flex mb-10 items-center justify-between border-b px-6 py-4 bg-white">
       <div className="text-xl font-bold">
-        AUTO <span className="text-gray-500">ULTIMATE</span>
+        MOTO <span className="text-gray-500">RENTALS</span>
       </div>
       <button className="bg-black text-white px-4 py-2 rounded-lg text-sm">
         PRO features
